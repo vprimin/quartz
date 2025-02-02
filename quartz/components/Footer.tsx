094c2eb140bc25e14ce27157be6005e41a7f297c
Slava Primin <primin@icloud.com>
2025-02-02 14:54:57 +0200

    footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,26 +1,28 @@
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+  function Footer({ displayClass }: QuartzComponentProps) {
     const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <small>
+            We can not change the past but we are can start today to make a better tomorrow..
+          </small>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
-              <a href={link}>{text}</a>
+              <small>
+                <a href={link}>{text}</a>
+              </small>
             </li>
           ))}
         </ul>
